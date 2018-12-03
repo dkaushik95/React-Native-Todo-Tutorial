813771061f77bb1512e9261b945099697e0f700a
Dishant Kaushik <dkaushik95@gmail.com>
2018-12-03 15:57:43 -0500

    added text input and button

--- code/code/app/screens/ToDoApp.js
+++ code/code/app/screens/ToDoApp.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput, Button } from 'react-native';
 import Heading from '../components/Heading';
 
 export default class ToDoApp extends Component {
+
+
+    onAdd(){
+        console.log('Add was pressed!');
+    }
+
     render(){
         return (
             <View>
                 <Heading />
-                
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
+                }}>
+                    <TextInput
+                        placeholder='Enter your text here'
+                        style={{
+                            fontSize: 20,
+                            borderBottomColor: 'grey',
+                            borderBottomWidth: 1,
+                            padding: 10,
+                            flex: 2
+                        }}
+                        numberOfLines={3}
+                    />
+                    <Button onPress={this.onAdd} title='Add todo' />
+                </View>
             </View>
         );
     }
