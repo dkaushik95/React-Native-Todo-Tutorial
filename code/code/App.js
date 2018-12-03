4966f6461847f54555da437a2ee5181338fd639e
Dishant Kaushik <dkaushik95@gmail.com>
2018-12-03 15:17:00 -0500

    added state

--- code/code/App.js
+++ code/code/App.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 import { Text, View, SafeAreaView } from 'react-native';
 
-class Greeting extends Component {
-	render() {
-		return (
-			<View>
-        <Text>Welcome, {this.props.name}!</Text>
+class Blink extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      blinking: false
+    };
+
+    setInterval(() => {
+      this.setState({
+        blinking: !this.state.blinking
+      })
+    }, 500);
+  }
+
+  render(){
+    return (
+      <View>
+        <Text style={{
+          color: this.state.blinking ? 'white' : 'black',
+          backgroundColor: this.state.blinking ? 'black' : 'white',
+          fontSize: 30
+        }}>{this.props.children}</Text>
       </View>
-		);
-	}
+    );
+  }
 }
 
 export default class App extends Component {
 	render() {
 		return (
 			<SafeAreaView>
-        <Greeting name='Dishant' />
-        <Greeting name='Lauren' />
+        <Blink>Hello</Blink>
+        <Blink>Hello</Blink>
+        <Blink>Hello</Blink>
       </SafeAreaView>
 		);
 	}
